24d2313280f638785045791b6cbb23e9413df88a
Eric Parsley <ericiparsley@hotmail.com>
2025-06-24 05:08:49 -0700

    Update Android namespace, applicationId, and SDK versions
    Changed the namespace and applicationId to use lowercase and match the package naming convention. Updated compileSdk and targetSdk to 35 for compatibility with the latest Android SDK.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,8 +6,8 @@ plugins {
 }
 
 android {
-    namespace = "com.WoofahRayetCode.GroceryGuardian"
-    compileSdk = flutter.compileSdkVersion
+    namespace = "com.woofahrayetcode.groceryguardian"
+    compileSdk = 35
     ndkVersion = "27.0.12077973"
 
     compileOptions {
@@ -21,11 +21,11 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.grocery_guardian"
+        applicationId = "com.woofahrayetcode.groceryguardian"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
